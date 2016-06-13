176004e6a7fb678a737e3332260141ec7b504433
Benjamin Logue and Maldon Meehan <maldon@>
2016-06-13 08:20:06 -0700

    it will add address book html, javascript and css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,33 @@
-/*This file is for your custom js.  All yours*/
+//business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
 
-// Calls input from form-input.html
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 
-$(document).ready(function(){
-
-  $("#input").submit(function(event){
+// user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
-    var input = ($("#blank").val());
 
-    var output = "";
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
 
-    $('#output').text(output);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
   });
 });
